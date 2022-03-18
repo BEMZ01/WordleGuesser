851540e36a3f726585881348d9b5f2507ba6a3a7
BEMZlabs <hello@bemz.info>
2022-03-18 08:48:05 +0000

    choose your own word for the AI to guess

--- main.py
+++ main.py
@@ -1,21 +1,96 @@
+#############################
+#                           #
+#  COPYRIGHT BEMZlabs 2022  #
+#    See LICENSE.MD for     #
+#         more info         #
+#                           #
+#############################
+
+"""
+Don't worry too much about the scary copyright at the top, it just allows me to claim that this is my work!
+If you are unsure weather your use falls out of the copyright license please email me (hello@bemz.info).
+"""
+
 import random
-import re
-from pprint import pprint
+import sys
+import os
 
 
-def ai(word_list, previous_words=None):
-    # using regular expressions, attempt to guess the word upon smart things
-    if previous_words == None:
-        return random.choice(word_list[random.randint(0, int(len(word_list)/2)):int(len(word_list))])
-    return 0
-
-
-def get_score(word):
-    score = 0
-    for x in range(len(ALPHABET)):
-        print(word, score)
-        score += word.count(ALPHABET[x]) * (abs(y - 28))  # give score based on frequency of common letters in alphabet
-    return word_score
+def ai(word_list, scores, previous_words, previous_words_info=None):
+    # using computer magic and wizardry; make computer choose what the word is
+    doesnt_contain = []
+    yellow_contain = []
+    green_contain = []
+    try:
+        with open("storage/ai", "r") as file:
+            file = file.readlines()
+            doesnt_contain = file[0].replace("\n", "").split("|")
+            temp = file[1].replace("\n", "").split("|")
+            for x in range(len(temp)):
+                yellow_contain.append(temp[x].split("-"))
+            temp = file[2].replace("\n", "").split("|")
+            for x in range(len(temp)):
+                green_contain.append(temp[x].split("-"))
+    except IndexError:
+        pass
+    if previous_words_info is None:
+        return [random.choice(word_list[random.randint(0, int(len(word_list) / 2)):int(len(word_list))]),]
+    for x in range(len(previous_words_info)):
+        word = previous_words_info[x]
+        for y in range(len(word)):
+            if word[y] == "N":
+                if previous_words[x][y] not in doesnt_contain:
+                    doesnt_contain.append(previous_words[x][y])
+            elif word[y] == "Y":
+                if [previous_words[x][y], y] not in yellow_contain:
+                    yellow_contain.append([previous_words[x][y], y])
+            elif word[y] == "G":
+                if [previous_words[x][y], y] not in green_contain:
+                    green_contain.append([previous_words[x][y], y])
+    possible_words = []
+    possible_words_score = []
+    for x in range(len(word_list)):  # THE WORD
+        score = 0
+        if word_list[x] in previous_words:
+            score -= 100
+        for no in doesnt_contain:
+            if no in word_list[x]:
+                score -= 2
+        for yellow in yellow_contain:
+            if yellow[0] in word_list[x]:
+                if yellow[0] != word_list[x][int(yellow[1])]:
+                    score += 5
+                else:
+                    score -= 10
+        for green in green_contain:
+            if green[0] == word_list[x][int(green[1])]:
+                score += 25
+        if score > 1:
+            possible_words.append(word_list[x])
+            possible_words_score.append(score)
+    with open("storage/ai", "w") as file:
+        out = ""
+        for word in doesnt_contain:
+            out = out + word + "|"
+        out = out.rstrip(out[-1]) + "\n"
+        for x in [green_contain, yellow_contain]:
+            for word in x:
+                out = out + str(word[0]) + "-" + str(word[1]) + "|"
+            out = out.rstrip(out[-1]) + "\n"
+        file.write(out)
+    sorted_words = [x for _, x in sorted(zip(possible_words_score, possible_words), reverse=True)]
+    if len(sorted_words) == 0:
+        return [random.choice(word_list[random.randint(0, int(len(word_list) / 2)):int(len(word_list))]),]
+    highest = 0
+    end_list = []
+    for x in range(len(sorted_words)):
+        if possible_words_score[x] > highest:
+            highest = possible_words_score[x]
+    print("AI: Stability = " + str(highest))
+    for x in range(len(sorted_words)):
+        if possible_words_score[x] == highest:
+            end_list.append(possible_words[x])
+    return end_list
 
 
 def display(attempts, cword):
@@ -40,43 +115,112 @@ def display(attempts, cword):
                 words[c].append("N")
     return words
 
+
+def game(player, correct_word=None):
+    with open("possible", "r") as file:
+        file = file.readlines()
+        for x in range(len(file)):
+            file[x] = str(file[x]).replace("\n", "").upper()
+            score.append(0.00)
+    for x in range(len(file)):
+        word_score = 0
+        for y in range(len(ALPHABET)):
+            word_score += file[x].count(ALPHABET[y]) * (abs(y - 28))
+            score[x] = word_score
+    sorted_list = [x for _, x in sorted(zip(score, file), reverse=True)]
+    with open("storage/ai", "w+"):
+        pass
+    if not player:
+        sys.stdout = open(os.devnull, 'w')
+    else:
+        sys.stdout = sys.__stdout__
+    if correct_word:
+        pass
+    else:
+        correct_word = random.choice(file)
+    guesses = 0
+    inputs = []
+    while guesses != 6:
+        history = display(inputs, correct_word)
+        print("Guess " + str(guesses + 1) + "/6")
+        ai_result = ai(sorted_list, score, inputs, history)
+        print("AI suggestion 1st: " + ai_result[0])
+        print(len(ai_result))
+        if len(ai_result) > 2:
+            print("AI suggestion 2nd: " + ai_result[1])
+            print("AI suggestion 3rd: " + ai_result[2])
+        if player:
+            inp = input("\n:> ").upper()
+            if inp not in sorted_list:
+                while 1:
+                    inp = input("\n:> ").upper()
+                    if inp in sorted_list:
+                        break
+        else:
+            inp = ai_result[0]
+        inputs.append(inp)
+        if inp == correct_word:
+            sys.stdout = sys.__stdout__
+            return True, guesses+1
+        guesses += 1
+    sys.stdout = sys.__stdout__
+    return False, -1
+
+
+def mainmenu():
+    menu = input("Main Menu.\n1. Human\n2. Emulate\n3. Chose your own word\n\n>: ")
+    while 1:
+        if menu.isnumeric() and menu in ["1", "2", "3"]:
+            menu = int(menu)
+            break
+        else:
+            menu = input("Main Menu.\n1. Human\n2. Emulate\n\n>: ")
+    print("\n\n")
+    if menu == 1:
+        print("\n\n")
+        results.append(game(True))
+    if menu == 2:
+        emulate = input("How many games would you like to emulate?\n\n>: ")
+        while 1:
+            if emulate.isdigit() and int(emulate) > 0:
+                break
+            else:
+                emulate = input("How many games would you like to emulate?\n\n>: ")
+        print("NOTE: You will not see any game CUI! Please be patient.")
+        for x in range(int(emulate)):
+            print("Now emulating game " + str(x + 1) + "/" + str(emulate))
+            try:
+                results.append(game(False))
+            except Exception as ExceptionError:
+                sys.stdout = sys.__stdout__
+                print("Exception Raised! " + str(ExceptionError) + " \\\\ Skipping!")
+    if menu == 3:
+        word_choice = input("Answer word \n>: ").upper()
+        while 1:
+            if len(word_choice) != 5:
+                word_choice = input("Answer word\n>: ").upper()
+            else:
+                break
+        results.append(game(True, word_choice))
+    won = 0
+    lost = 0
+    score = 0
+    for x in range(len(results)):
+        if results[x][0]:
+            won += 1
+            score += int(results[x][1])
+        else:
+            lost += 1
+    score = float(score / len(results))
+    percentage_won = float((won / (won + lost)) * 100)
+    print("\n\nWon games: " + str(won) + "\nLost games: " + str(lost) + "\nPercent won: " + str(
+        percentage_won) + "\nAverage score: " + str(score))
+
+
 ALPHABET = ["E", "T", "A", "O", "I", "N", "S", "R", "H", "D", "L", "U", "C", "M", "F", "Y", "W", "G", "P", "B", "V",
             "K", "X", "Q", "J", "Z"]
 score = []
 
-with open("possible", "r") as file:
-    file = file.readlines()
-    for x in range(len(file)):
-        file[x] = str(file[x]).replace("\n", "").upper()
-        score.append(0.00)
-for x in range(len(file)):
-    word_score = 0
-    for y in range(len(ALPHABET)):
-        word_score += file[x].count(ALPHABET[y]) * (abs(y - 28))
-        score[x] = word_score
-sorted_list = [x for _, x in sorted(zip(score, file), reverse=True)]
-print(sorted_list)
-print("Loaded words, total " + str(len(file)))
-print("\n")
-correct_word = random.choice(file)
-#print(correct_word)
-guesses = 0
-inputs = []
-while guesses != 6:
-    history = display(inputs, correct_word)
-    print(history)
-    print(correct_word)
-    print("Guess " + str(guesses + 1) + "/6")
-    ai(sorted_list, history)
-    inp = input("\n:> ").upper()  # replace with fancy ml code
-    # end fancy ml code
-    inputs.append(inp)
-    if inp == correct_word:
-        print("Yay, game won :)")
-        raise Exception("Crash")
+results = []
 
-
-    # if guesses = 6 end game
-    # if input = correct_word end game
-    guesses += 1
-raise Exception("You didnt get the answer lol")
+mainmenu()
